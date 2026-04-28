0bb409d8a7be3dbd1c9d7a14a278f72479cffd17
Apurbo Bhaket <161101753+Apurbo77@users.noreply.github.com>
2026-04-28 18:21:10 +0600

    react

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./DurgaPuja2026.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
